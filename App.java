8b743389f1b744e9a5e299c2e7ad3c1bca16f6fb
cleuber979 <105437987+cleuber979@users.noreply.github.com>
2023-01-16 09:39:35 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) {
+
+        int saldoAtual = 100;
+        int valorSolicitado = 40;
+
+        if (valorSolicitado > saldoAtual) {
+
+            System.out.println("Saldo insuficiente o valor solicitado e maior do que o valor disponivel");
+            System.out.println(" R$ " + valorSolicitado + "  e maior  do que seu saldo em conta  R$ " + saldoAtual);
+        } else {
+            saldoAtual = saldoAtual - valorSolicitado;
+
+            System.out.println("Saque realizado com sucesso  ");
+
+            System.out.println("Seu novo saldo é de R$  " + saldoAtual);
+        }
+
+        System.out.println("Seu saldo disponivel  é R$ " + saldoAtual);
+    }
+}
